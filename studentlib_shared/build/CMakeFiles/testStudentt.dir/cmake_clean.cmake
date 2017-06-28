92d1b84491a15e87f6338733a7456aa409fb570c
yuyang01 <alwyuyang@gmail.com>
2017-06-28 15:27:34 +0200

    cmake

---
+++ studentlib_shared/build/CMakeFiles/testStudentt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testStudentt.dir/src/Student.cpp.o"
+  "libtestStudentt.pdb"
+  "libtestStudentt.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testStudentt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
